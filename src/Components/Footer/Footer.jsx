03f625069428e034da0f1f1af39683efb70cdb5f
Tarun Tiwari <tiwaritarun700@gmail.com>
2024-05-23 10:43:34 +0530

    rte change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ export function Footer() {
             <div className="w-full p-6 md:w-1/2 lg:w-5/12">
                 <div className="flex h-full flex-col justify-between">
                     <div className="mb-4 inline-flex items-center">
-                        <Logo classname='h-[200px] w-[200px] mr-20' />
+                        <Logo classname='h-[210px] w-[210px] mr-20' />
                     </div>
                     <div>
                         <p className="text-sm mr-14 ">
--- src/Components/RTE.jsx
+++ src/Components/RTE.jsx
@@ -1,51 +1,67 @@
-import React from 'react'
-import { Controller } from 'react-hook-form'
-import {Editor} from '@tinymce/tinymce-react'
-export default function RTE({name, control,label, defaultValue = ""}) {
+import React from 'react';
+import { Controller } from 'react-hook-form';
+import { Editor } from '@tinymce/tinymce-react';
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  const handleEditorChange = (content, editor) => {
+    editor.getBody().setAttribute('dir', 'ltr'); // Enforce LTR direction on content change
+    onChange(content);
+  };
+
   return (
     <div className='w-full'>
-    {label && <label className='inline-block mb-1 pl-1 '>{label}</label>}
-    <Controller
-    name = {name || "content"}
-    control = {control}
-    render ={({field : {onChange}})=>{
-       return <Editor
-       apiKey='ba5uegymspq869k9ppqqmy7iyrv0x343usoozmiybsy4folu'
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => {
+          return (
+            <Editor
+              apiKey='ba5uegymspq869k9ppqqmy7iyrv0x343usoozmiybsy4folu'
+              initialValue={defaultValue}
+              init={{
+                height: 500,
+                menubar: true,
+                plugins: [
+                  "image",
+                  "advlist",
+                  "autolink",
+                  "lists",
+                  "link",
+                  "image",
+                  "charmap",
+                  "preview",
+                  "anchor",
+                  "searchreplace",
+                  "visualblocks",
+                  "code",
+                  "fullscreen",
+                  "insertdatetime",
+                  "media",
+                  "table",
+                  "code",
+                  "help",
+                  "wordcount",
+                  "anchor",
+                ],
+                toolbar:
+                  "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px; direction: ltr; }",
+                directionality: 'ltr',
+                setup: (editor) => {
+                  editor.on('init', () => {
+                    editor.getBody().setAttribute('dir', 'ltr');
+                  });
+                  editor.on('change', (e) => {
+                    editor.getBody().setAttribute('dir', 'ltr');
+                  });
+                }
+              }}
+              onEditorChange={handleEditorChange}
+            />
+          );
         }}
-        onEditorChange={onChange}
-        />
-    }}
-    />
+      />
     </div>
-  )
+  );
 }
