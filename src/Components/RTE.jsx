9a3cd65c96a9bac54c2dd3d49121dd0f7487fc50
Tarun Tiwari <tiwaritarun700@gmail.com>
2024-05-23 12:05:35 +0530

    rte

--- src/Components/RTE.jsx
+++ src/Components/RTE.jsx
@@ -3,9 +3,10 @@ import { Controller } from 'react-hook-form';
 import { Editor } from '@tinymce/tinymce-react';
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
-  const handleEditorChange = (content, editor) => {
-    editor.getBody().setAttribute('dir', 'ltr'); // Enforce LTR direction on content change
-    onChange(content);
+  const handleEditorChange = (content, editor, onChange) => {
+    // Enforce LTR direction on content change
+    editor.getBody().setAttribute('dir', 'ltr');
+    onChange(content); // Update react-hook-form state
   };
 
   return (
@@ -14,53 +15,36 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
       <Controller
         name={name || "content"}
         control={control}
-        render={({ field: { onChange } }) => {
-          return (
-            <Editor
-              apiKey='ba5uegymspq869k9ppqqmy7iyrv0x343usoozmiybsy4folu'
-              initialValue={defaultValue}
-              init={{
-                height: 500,
-                menubar: true,
-                plugins: [
-                  "image",
-                  "advlist",
-                  "autolink",
-                  "lists",
-                  "link",
-                  "image",
-                  "charmap",
-                  "preview",
-                  "anchor",
-                  "searchreplace",
-                  "visualblocks",
-                  "code",
-                  "fullscreen",
-                  "insertdatetime",
-                  "media",
-                  "table",
-                  "code",
-                  "help",
-                  "wordcount",
-                  "anchor",
-                ],
-                toolbar:
-                  "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
-                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px; direction: ltr; }",
-                directionality: 'ltr',
-                setup: (editor) => {
-                  editor.on('init', () => {
-                    editor.getBody().setAttribute('dir', 'ltr');
-                  });
-                  editor.on('change', (e) => {
-                    editor.getBody().setAttribute('dir', 'ltr');
-                  });
-                }
-              }}
-              onEditorChange={handleEditorChange}
-            />
-          );
-        }}
+        defaultValue={defaultValue}
+        render={({ field: { onChange, value } }) => (
+          <Editor
+            apiKey='ba5uegymspq869k9ppqqmy7iyrv0x343usoozmiybsy4folu'
+            initialValue={defaultValue}
+            value={value || ""}
+            init={{
+              height: 500,
+              menubar: true,
+              plugins: [
+                "advlist", "autolink", "lists", "link", "image", "charmap", "preview",
+                "anchor", "searchreplace", "visualblocks", "code", "fullscreen",
+                "insertdatetime", "media", "table", "help", "wordcount", "anchor"
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+              content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px; direction: ltr; }",
+              directionality: 'ltr',
+              setup: (editor) => {
+                editor.on('init', () => {
+                  editor.getBody().setAttribute('dir', 'ltr');
+                });
+                editor.on('change', () => {
+                  editor.getBody().setAttribute('dir', 'ltr');
+                });
+              }
+            }}
+            onEditorChange={(content, editor) => handleEditorChange(content, editor, onChange)}
+          />
+        )}
       />
     </div>
   );
