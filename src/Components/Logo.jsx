fb72c1f9fdce7be9a0a64dab8b49fc6095724097
Tarun Tiwari <tiwaritarun700@gmail.com>
2025-01-20 21:59:45 +0530

    minor fixes

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -6,7 +6,7 @@ export function Logo({classname=""}) {
     return (
         <>
            <div class=" space gap-y-8 ">
-        <img src={logoimage} alt="logo" className={`rounded-full ml-[100px] h-[65px] w-[65px] border border-solid border-t-2 border-l-2 border-b-2 border-r-2  border-gray-200 ${classname} `}/>
+        <img src={logoimage} alt="logo" className={`rounded-full  h-[65px] w-[65px] border border-solid border-t-2 border-l-2 border-b-2 border-r-2  border-gray-200 ${classname} `}/>
        </div>
         </>
     )
